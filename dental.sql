deacb5dd46aaec51ff50dd03fe0e48d47741db87
Anurag <anuragrajanp@gmail.com>
2023-08-21 15:28:39 +0530

    commit

---
+++ dental.sql
@@ -0,0 +1,15 @@
+create table patients (patientid int primary key, name varchar(20), phonenumber varchar(10), email varchar(20));
+
+create table appointments (appointmentid int primary key, patientid int, foreign key (patientid) references patients(patientid));
+
+create table dentists (dentistid int primary key,name varchar(15),phonenumber int);
+
+create table procedures (procedureid int primary key, procedurename varchar(20),cost decimal(10,2));
+
+create table payments (paymentid int primary key, patientid int, appointmentid int,paymentdate date, amount decimal(10,2),foreign key(patientid) references patients(patientid), foreign key(appointmentid) references appointments(appointmentid));
+
+create table medicalhistory (medicalhistoryid int primary key, patientid int, datevisited date, foreign key(patientid) references patients(patientid));
+
+create table staff (staffid int primary key, name varchar(20), position varchar(20), phonenumber varchar(10));
+
+insert into patients (1,"anvin"9778675656,"anvin@email.com"), 
